2792d95f78cad2cc0e17741476de54a48b31c16c
Priya <priyaksingh@gmail.com>
2016-03-17 13:59:59 +0000

    second commit

--- JavaProblem/src/main/java/SuperMarketPlusPlus.java
+++ JavaProblem/src/main/java/SuperMarketPlusPlus.java
@@ -3,108 +3,127 @@ import java.util.List;
 
 public class SuperMarketPlusPlus {
 
-	private static List<Item> items = null;
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	private List<Item> items = new ArrayList<Item>();
 
-		System.out.println("Starting Supermarket Plus Plus");
-		items = new ArrayList<Item>();
-		items.add(new Item(ItemNames.THERMAL_VEST.getItenName(), 10, 20));
-		items.add(new Item(ItemNames.AGED_BRIE.getItenName(), 2, 0));
-		items.add(new Item(ItemNames.CHICKEN.getItenName(), 5, 7));
-		items.add(new Item(ItemNames.SULFURAS.getItenName(), 0, 80));
-		items.add(new Item(ItemNames.BACKSTAGE_PASSES.getItenName(), 15, 20));
-		items.add(new Item(ItemNames.GINGER_CAKE.getItenName(), 3, 6));
-		/**
-		 * START - Changes by Priya. Added new Item Organic Banana.
-		 */
-		items.add(new Item(ItemNames.ORGANIC_BANANA.getItenName(), 5, 10));
-		/**
-		 * END - Changes by Priya. Added new Item Organic Banana.
-		 */
-
-		updateQuality();
-	}
-
-	public static void updateQuality1() {
-		for (int i = 0; i < items.size(); i++) {
-			switch (ItemNames.valueOf(items.get(i).getName())) {
-			//To Do
-			
-
-			}
-		}
-	}
-	
-	
-	public static void updateQuality() {
-		for (int i = 0; i < items.size(); i++) {
-			if ((!ItemNames.AGED_BRIE.getItenName().equals(items.get(i).getName()))
-					&& !ItemNames.BACKSTAGE_PASSES.getItenName().equals(items.get(i).getName())) {
-				if (items.get(i).getQuality() > 0) {
-					if (!ItemNames.SULFURAS.getItenName().equals(items.get(i).getName())
-							&& !ItemNames.ORGANIC_BANANA.getItenName().equals(items.get(i).getName())) {
-						items.get(i).setQuality(items.get(i).getQuality() - 1);
-					} else if (ItemNames.ORGANIC_BANANA.getItenName().equals(items.get(i).getName())) {
-						items.get(i).setQuality(items.get(i).getQuality() - 2);
-					}
-				}
-			} else {
-				if (items.get(i).getQuality() < 50) {
-					items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-					if (ItemNames.BACKSTAGE_PASSES.getItenName().equals(items.get(i).getName())) {
-						if (items.get(i).getSellIn() < 11) {
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(items.get(i).getQuality() + 1);
-							}
-						}
-
-						if (items.get(i).getSellIn() < 6) {
-							if (items.get(i).getQuality() < 50) {
-								items.get(i).setQuality(items.get(i).getQuality() + 1);
-							}
-						}
-					}
-				}
-			}
-
-			if (!ItemNames.SULFURAS.getItenName().equals(items.get(i).getName())) {
-				items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-			}
-
-			if (items.get(i).getSellIn() < 0) {
-				if (!ItemNames.AGED_BRIE.getItenName().equals(items.get(i).getName())) {
-					if (!ItemNames.BACKSTAGE_PASSES.getItenName().equals(items.get(i).getName())) {
-						if (items.get(i).getQuality() > 0) {
-							if (!ItemNames.SULFURAS.getItenName().equals(items.get(i).getName())
-									&& !ItemNames.ORGANIC_BANANA.getItenName().equals(items.get(i).getName())) {
-								items.get(i).setQuality(items.get(i).getQuality() - 1);
-							} else if (ItemNames.ORGANIC_BANANA.getItenName().equals(items.get(i).getName())) {
-								items.get(i).setQuality(items.get(i).getQuality() - 2);
-							}
-						}
-					} else {
-						items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-					}
-				} else {
-					if (items.get(i).getQuality() < 50) {
-						items.get(i).setQuality(items.get(i).getQuality() + 1);
-					}
-				}
-			}
-		}
-	}
-
-	public static List<Item> getItems() {
+	public List<Item> getItems() {
 		return items;
 	}
 
-	public static void setItems(List<Item> items) {
-		SuperMarketPlusPlus.items = items;
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+	public void updateQuality1() {
+		for (int i = 0; i < items.size(); i++) {
+			if (items.get(i).getName() != null && !items.get(i).getName().isEmpty()) {
+				switch (ItemNames.valueOf(items.get(i).getName())) {
+				// To Do
+				case THERMAL_VEST:
+					decrementSellIn(items.get(i));
+				case AGED_BRIE:
+				case CHICKEN:
+				case BACKSTAGE_PASSES:
+				case GINGER_CAKE:
+				case ORGANIC_BANANA:
+
+				case SULFURAS:
+
+				}
+			}
+		}
+	}
+
+	public void updateQuality() {
+		for (int i = 0; i < items.size(); i++) {
+			if (items.get(i).getName() != null && !items.get(i).getName().isEmpty()) {
+				if (isQualityDegradationRequired(items.get(i))) {
+					degradeQuality(items.get(i));
+				} else {
+					upgradeQuality(items.get(i));
+				}
+				decrementSellIn(items.get(i));
+				updateQualityBasedOnSellin(items.get(i));
+			}
+		}
+	}
+
+	private int degradeQuality(Item item) {
+		if (!ItemNames.SULFURAS.getItenName().equals(item.getName())
+				&& !ItemNames.ORGANIC_BANANA.getItenName().equals(item.getName())) {
+			item.setQuality(item.getQuality() - 1);
+		} else if (ItemNames.ORGANIC_BANANA.getItenName().equals(item.getName())) {
+			item.setQuality(item.getQuality() - 2);
+		}
+		return item.getQuality();
+	}
+
+	private boolean isQualityDegradationRequired(Item item) {
+		boolean isDegradeQuality = false;
+		if ((!ItemNames.AGED_BRIE.getItenName().equals(item.getName()))
+				&& !ItemNames.BACKSTAGE_PASSES.getItenName().equals(item.getName()) && item.getQuality() > 0) {
+			isDegradeQuality = true;
+		}
+		return isDegradeQuality;
+	}
+
+	private int upgradeQuality(Item item) {
+		if (item.getQuality() < 50) {
+			item.setQuality(item.getQuality() + 1);
+			if (ItemNames.BACKSTAGE_PASSES.getItenName().equals(item.getName())) {
+				if (item.getSellIn() < 11) {
+					if (item.getQuality() < 50) {
+						item.setQuality(item.getQuality() + 1);
+					}
+				}
+				if (item.getSellIn() < 6) {
+					if (item.getQuality() < 50) {
+						item.setQuality(item.getQuality() + 1);
+					}
+				}
+			}
+		}
+		return item.getQuality();
+	}
+
+	private int decrementSellIn(Item item) {
+		if (isSellInDecrementRequired(item)) {
+			item.setSellIn(item.getSellIn() - 1);
+		}
+		return item.getSellIn();
+	}
+
+	private Item updateQualityBasedOnSellin(Item item) {
+		if (item.getSellIn() < 0) {
+			if (!ItemNames.AGED_BRIE.getItenName().equals(item.getName())) {
+				if (!ItemNames.BACKSTAGE_PASSES.getItenName().equals(item.getName())) {
+					if (item.getQuality() > 0) {
+						if (!ItemNames.SULFURAS.getItenName().equals(item.getName())
+								&& !ItemNames.ORGANIC_BANANA.getItenName().equals(item.getName())) {
+							item.setQuality(item.getQuality() - 1);
+						} else if (ItemNames.ORGANIC_BANANA.getItenName().equals(item.getName())) {
+							item.setQuality(item.getQuality() - 2);
+						}
+					}
+				} else {
+					item.setQuality(item.getQuality() - item.getQuality());
+				}
+			} else {
+				if (item.getQuality() < 50) {
+					item.setQuality(item.getQuality() + 1);
+				}
+			}
+		}
+		return item;
+	}
+
+	private boolean isSellInDecrementRequired(Item item) {
+		boolean isSellInDecrementRequired = false;
+		if (!ItemNames.SULFURAS.getItenName().equals(item.getName())) {
+			isSellInDecrementRequired = false;
+		} else {
+			isSellInDecrementRequired = true;
+		}
+		return isSellInDecrementRequired;
 	}
 
 }
--- JavaProblem/src/test/java/SuperMarketPlusPlusTest.java
+++ JavaProblem/src/test/java/SuperMarketPlusPlusTest.java
@@ -5,21 +5,96 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class SuperMarketPlusPlusTest {
 	
-	private static List<Item> items = new ArrayList<Item>();
-	private Item organicBanana = null;
+	private static List<Item> items = null;
+	private SuperMarketPlusPlus superMarket;
 	
+	@Before
+	 public void setup() {
+		items = new ArrayList<Item>();
+		superMarket = new SuperMarketPlusPlus();
+	}
+	
+	@After
+	public void destroy(){
+		items = null;
+	}
 	
 	@Test
 	public void testOrganicBananaDegradesTwiceAsFast() {
-		organicBanana = new Item(ItemNames.ORGANIC_BANANA.getItenName(),5,10);
+		Item organicBanana = new Item(ItemNames.ORGANIC_BANANA.getItenName(),5,10);
 		items.add(organicBanana);
-		SuperMarketPlusPlus.setItems(items);
-		SuperMarketPlusPlus.updateQuality();
+		superMarket.setItems(items);
+		superMarket.updateQuality();
 		assertEquals(8,organicBanana.getQuality());
+	}
+	
+	@Test
+	public void testThermalVest() {
+		Item thermalVest = new Item(ItemNames.THERMAL_VEST.getItenName(),10, 20);
+		items.add(thermalVest);
+		superMarket.setItems(items);
+		superMarket.updateQuality();
+		assertEquals(19,thermalVest.getQuality());
+	}
+	
+	@Test
+	public void testAgedBrieUpgrades() {
+		Item agedBrie = new Item(ItemNames.AGED_BRIE.getItenName(), 2, 0);
+		items.add(agedBrie);
+		superMarket.setItems(items);
+		superMarket.updateQuality();
+		assertEquals(1,agedBrie.getQuality());
+	}
+	
+	@Test
+	public void testChickenDegrades() {
+		Item chicken = new Item(ItemNames.CHICKEN.getItenName(), 5, 7);
+		items.add(chicken);
+		superMarket.setItems(items);
+		superMarket.updateQuality();
+		assertEquals(6,chicken.getQuality());
+	}
+	
+	@Test
+	public void testSulfurasNoChange() {
+		Item sulfuras = new Item(ItemNames.SULFURAS.getItenName(), 0, 80);
+		items.add(sulfuras);
+		superMarket.setItems(items);
+		superMarket.updateQuality();
+		assertEquals(80,sulfuras.getQuality());
+	}
+	
+	@Test
+	public void testGingerCakeDegrades() {
+		Item gingerCake = new Item(ItemNames.GINGER_CAKE.getItenName(), 3, 6);
+		items.add(gingerCake);
+		superMarket.setItems(items);
+		superMarket.updateQuality();
+		assertEquals(5,gingerCake.getQuality());
+	}
+	
+	@Test
+	public void testBackstagePassesUpgrades() {
+		Item backStagePasses = new Item(ItemNames.BACKSTAGE_PASSES.getItenName(), 15, 20);
+		items.add(backStagePasses);
+		superMarket.setItems(items);
+		superMarket.updateQuality();
+		assertEquals(21,backStagePasses.getQuality());
+	}
+	
+	@Test
+	public void testBackstagePassesUpgradesTwiceAsFast() {
+		Item backStagePasses = new Item(ItemNames.BACKSTAGE_PASSES.getItenName(), 5, 20);
+		items.add(backStagePasses);
+		superMarket.setItems(items);
+		superMarket.updateQuality();
+		assertEquals(23,backStagePasses.getQuality());
 	}
 }
